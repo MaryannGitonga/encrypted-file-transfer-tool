c89a195191ca20de4b3382a70c1ec7c2bd0e8020
MaryannGitonga <naygitonga4@gmail.com>
2023-06-02 16:05:49 +0300

    Specify explicit keys

--- keys.py
+++ keys.py
@@ -1,4 +1,3 @@
-import os
 
-KEY = os.urandom(16)
-INITIALIZATION_VECTOR = os.urandom(16) # adds randomness & uniqueness to the encryption process
+KEY = b"eErflQHaWY5zShde"
+INITIALIZATION_VECTOR = b"c25tuuKocii83z9R" # adds randomness & uniqueness to the encryption process
